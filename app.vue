5cab0cfcf30faf1897f24a93377206a40657bf8d
EJ Fox <ejfox@ejfox.com>
2023-02-01 13:54:00 -0500

    feat: add hello world

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Hello deploy</h1>
   </div>
 </template>
+<style>
+h1 {
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 8rem;
+}
+</style>
