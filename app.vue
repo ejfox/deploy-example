f333e1ed694c2f6eb955136dac666c16269da80f
EJ Fox <ejfox@ejfox.com>
2023-02-01 13:56:12 -0500

    feat: add button to change message

--- app.vue
+++ app.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-    <h1>Hello deploy</h1>
+    <h1>{{msg}}</h1>
+
+    <!-- when button is clicked, change message to 'hello world!' -->
+    <button @click="msg = 'hello world!'">change message</button>
+    
   </div>
 </template>
+<script setup>
+const msg = ref('Hello deploy')
+</script>
 <style>
 h1 {
   text-align: center;
